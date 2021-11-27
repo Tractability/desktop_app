f9beacbe0975fecc2472202d62d115d6b2924702
Nilesh Aditya <nileshaditya2000@gmail.com>
2021-11-28 01:47:44 +0530

    organing scrapper.js

--- scrapper.js
+++ scrapper.js
@@ -8,26 +8,36 @@ class Scrapper{
         this.visited = new Set();
         // this.visited = {};
         this.urls = [];
+        this.init();
+        // this.browser = null;
+    }
+    
+    async init(){
         const URL = this.link
         browser = await puppeteer.launch({
             headless: false,
             'ignoreHTTPSErrors': true
         })
+        await this.workerFuntion(this.link);
     }
 
-    async workerFuntion(){
+    async workerFuntion(url){
         try {
             const page = await browser.newPage()
     
-            await page.goto(URL)
+            await page.goto(url)
             await page.waitForSelector('a');
 
             this.urls = await page.$$eval('a', links => {
-                links = links.map(el => el.href)
+                links = links.map(el => {
+                    // this.workerFuntion(el.href);
+                    return el.href;
+                })
                 return links;
             });
     
             // this.allLinks = new Set(...urls);
+            console.log(this.urls);
         } catch (error) {
             console.error(error)
         }
@@ -43,7 +53,7 @@ class Scrapper{
         }
     }
 
-    destructor(){
+    async destructor(){
         await browser.close();
     }
 
@@ -57,4 +67,4 @@ class Scrapper{
 }
 
 let scrape = new Scrapper('https://www.digitalocean.com/community/tutorials/how-to-scrape-a-website-using-node-js-and-puppeteer');
-scrape.workerFuntion();
+// scrape.workerFuntion();
